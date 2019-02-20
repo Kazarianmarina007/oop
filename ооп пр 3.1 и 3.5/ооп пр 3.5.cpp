b12b6ae4e8ae6853cd759c54628fdcb09f7622e1
Marinka <Kazarianmarina007@gmail.com>
2019-02-20 11:17:47 +0300

    biblio

---
+++ ооп пр 3.1 и 3.5/ооп пр 3.5.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "ооп пр 3.1.cpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct biblio {
+	string adress;
+	string nazvanie;
+	int god_otkr;
+	knigga k1[10];
+};
+
