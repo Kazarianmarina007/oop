1064291fbd0a7f1199bf2782b664d18f0da84bf2
Marinka <Kazarianmarina007@gmail.com>
2019-02-20 12:50:08 +0300

    заполненные 3.1 и 3.5

--- ооп пр 3.1 и 3.5/ооп пр 3.1.cpp
+++ ооп пр 3.1 и 3.5/ооп пр 3.1.cpp
@@ -9,3 +9,14 @@ struct Knigga {
 	int god_izdania;
 	string ima_kniggi;
 };
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Knigga k1 = {
+		510,(string) "Кинг",1990,(string)" Сияние"
+	};
+	Knigga k2 = {
+		400,(string) "Роулинг",2007,(string) "Гарри П"
+	};
+	system("pause");
+	return 0;
+}
--- ооп пр 3.1 и 3.5/ооп пр 3.5.cpp
+++ ооп пр 3.1 и 3.5/ооп пр 3.5.cpp
@@ -10,4 +10,15 @@ struct biblio {
 	int god_otkr;
 	knigga k1[10];
 };
-
+int main() {
+	biblio b1 = {
+		(string)"2 улица",(string)"крутая",2001,{
+			{
+		510,(string) "Кинг",1990,(string)" Сияние"};
+	{
+		400,(string) "Роулинг",2007,(string) "Гарри П"};
+												}
+	};
+	system("pause");
+	return 0;
+}
