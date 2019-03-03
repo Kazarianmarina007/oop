0c10deb3ee480e244407d43962f745a2531eacda
Marinka <Kazarianmarina007@gmail.com>
2019-03-03 20:31:28 +0300

    7.1

---
+++ пр 7/ооп пр 7.1.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class animal {
+public:
+	string klichka;
+	int ves;
+	int rost;
+	virtual void zvuk() = 0;
+};
+class kot :public animal {
+public:
+	string kon = "lapki";
+	kot(string kl,int v,int r) { 
+		kl= klichka;
+		v = ves;
+		r = rost;
+	}
+	void zvuk() {
+		cout<<"рррр, € хочу рычать, а не м€укать";
+	}
+};
+class sob :public animal {
+public:
+	string kon = "lapki";
+	sob(string kl, int v, int r) {
+		kl = klichka;
+		v = ves;
+		r = rost;
+	}
+	void zvuk() {
+		cout << "эта кошка мен€ достала";
+	}
+};
+
+class pop :public animal {
+public:
+	string kon = "krylia";
+	pop(string kl, int v, int r) {
+		kl = klichka;
+		v = ves;
+		r = rost;
+	}
+	void zvuk() {
+		cout << "хочу на карнавал в –ио, а не вот это вот все";
+	}
+};
+
+
