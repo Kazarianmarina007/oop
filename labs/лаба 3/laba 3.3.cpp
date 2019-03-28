5ea94a0e64433dd6d3cd607a97792d25f2047a5f
Marina <kazarianmarina007@gmail.com>
2019-03-28 12:13:41 +0300

    laba 3.3

--- labs/лаба 3/laba 3.3.cpp
+++ labs/лаба 3/laba 3.3.cpp
@@ -8,30 +8,38 @@ private:
 	float re;
 	float im;
 public:
-	void sch(float re, float im) {
-		this->re = re;
-		this->im = im;
+	Complex(float a, float b) {
+		re = a;
+		im = b;
 	}
 	float mod() {
 		return sqrt(re*re + im * im);
 	};
 	float arg() {
-		float t,m;
-		m = sqrt(re*re + im * im);
-		t = atan(im / re);
-		return t;
+		float res;
+		if (re > 0) { res = atan(im / re); }
+		else if (re == 0) {
+			if (re > 0) { res = 3.14 / 2; }
+			else { res = 3.14 / 2; }
+		}
+		else {
+			if (im >= 0) { res = 3.14 + atan(im / re); }
+			else { res = -3.14 + atan(im / re); }
+		}
+		return res;
 	}
 
 	void pr() {
-		cout <<" modul "<< this->mod() << " argument " << this-> arg() << endl;
+		cout << " modul " << this->mod() << " argument " << this->arg() << endl;
 	}
 };
 
 int main()
 {
-	Complex c1,c2;
-	c1.sch(-10, 20);
+	Complex c1(-10, 20);
+	Complex c2(-20, 90);
 	c1.pr();
+	c2.pr();
 	system("pause");
 	return 0;
 }
