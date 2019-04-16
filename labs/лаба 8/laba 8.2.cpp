b49fe233e806bcd57ed174ef5727420335e1fdb2
Marina <kazarianmarina007@gmail.com>
2019-04-16 21:21:36 +0300

    ispr

--- labs/лаба 8/laba 8.2.cpp
+++ labs/лаба 8/laba 8.2.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main()
 {
 	list<Complex> ls;
-	int count;
+	int kol;
 	cout << "kolicestvo compleksnyh chisel: ";
-	cin >> count;
-	for (int i = 0; i < count; i++) {
-		Complex input_number(0, 0);
+	cin >> kol;
+	for (int i = 0; i < kol; i++) {
+		Complex nom(0, 0);
 		cout << "vvedite copmleksnoe chislo (vvodite cherez probel snachala deystvitelnuu,a potom mnimuu chasti):";
-		cin >> input_number;
-		ls.push_back(input_number);
+		cin >> nom;
+		ls.push_back(nom);
 	}
 	return 0;
 }
